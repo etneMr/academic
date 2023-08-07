46d008b285b06ca6674ac7459b5f39cf6c99c4d4
etneMr <91597719+etneMr@users.noreply.github.com>
2023-08-07 10:37:12 +0700

    master food main page ui

---
+++ app/finance/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './finance.css'
+
+const Finance = () => {
+  return (
+    <div>Finance</div>
+  )
+}
+
+export default Finance
---
+++ app/lastest-activity/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './lastest-activity.css'
+
+const LastestActivy = () => {
+  return (
+    <div>LastestActivy</div>
+  )
+}
+
+export default LastestActivy 
